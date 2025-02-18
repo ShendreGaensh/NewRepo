c4bb4c4b9246078dab6a271386a29759b005b436
shendreganesh <iamganessh7@gmail.com>
2025-02-18 16:17:29 +0530

    Added new sample class

---
+++ SampleProjectForTestNG/src/test/java/samplePackage/Sample3.java
@@ -0,0 +1,15 @@
+package samplePackage;
+
+import org.testng.annotations.Test;
+
+import com.Vtiger.BaseClassUtility.BaseClassUtility;
+
+public class Sample3 extends BaseClassUtility{
+	
+	@Test
+	public void sample5 () {
+		System.out.println("sample5");
+	}
+	
+
+}
